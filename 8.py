5ff23d0070403b455b49665a52e889f03798ebee
grocholskaaleksandra <56762756+grocholskaaleksandra@users.noreply.github.com>
2019-11-14 21:26:13 +0100

    Delete 8.py

--- 8.py
+++
@@ -1,8 +0,0 @@
-#utworz program ktory wylaczy sie po nacisnieciu przez uzytkowanika konktetnego przycisku
-
-zakonczyc = 'n'
-
-while zakonczyc != 't':
-    print('Jesli chcesz zakonczyc program kliknij "t"')
-    zakonczyc = input()
-
